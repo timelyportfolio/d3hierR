fd34f1c17464d98551ed441ca30edbac2bf873a7
timelyportfolio <kent.russell@timelyportfolio.com>
2017-07-17 12:06:41 -0500

    add color example

--- inst/examples/examples.R
+++ inst/examples/examples.R
@@ -5,17 +5,60 @@ library(treemap)
 library(d3hierR)
 
 rhd <- random.hierarchical.data()
+# make up a fake column for coloring
+rhd$spread <- runif(nrow(rhd), -5, 10)
 
 rhd_nest <- d3_nest(
   rhd,
-  value_cols = "x"
+  value_cols = c("x", "spread")
 )
 
-tree(
+tr <- tree(
   rhd_nest,
   sizeField = "x",
+  valueField = "spread",
   paddingOuter = 10
 )
+
+# use tasks to color by spread
+#   for now just do leaf level
+#   but can easily accommodate all levels
+tr$x$tasks = list(
+  htmlwidgets::JS(
+"
+function() {
+
+debugger;
+  var el = d3.select(this.el);
+  var cells = el.selectAll('g.cell rect');
+  var valueField = this.x.valueField;
+  if(!valueField) {
+    return
+  }
+
+  // first get min and max
+  var extent = d3.extent(cells.data(), function(d) {
+    return d.data[valueField];
+  });
+
+  var color = d3.scaleLinear()
+    .range(['red','white','blue'])
+    .domain([extent[0],0,extent[1]])
+    .interpolate(d3.interpolateLab);
+
+  cells
+    .style('fill', function(d) {
+      // just the leaves
+      if (d.height === 0) {
+        return color(d.data[valueField]);
+      }
+    })
+}
+"
+  )
+)
+
+tr
 
 tree(
   rhd_nest,
